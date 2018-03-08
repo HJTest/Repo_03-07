042130578a1db6ddcc1a4ba0def1a70a7f1da61e
HJTest <36950275+HJTest@users.noreply.github.com>
2018-03-08 14:09:09 +0530

    Add files via upload

---
+++ Progm3.java
@@ -0,0 +1,13 @@
+package com.progms;
+
+import java.util.Scanner;
+
+public class Progm3 {
+
+	public static void main(String[] args) {
+		String rev = null;
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
