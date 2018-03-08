826598be6d9ed56eec3462d58cef09ed6f4e14ee
HJTest <36950275+HJTest@users.noreply.github.com>
2018-03-08 12:55:09 +0530

    Add files via upload

---
+++ Progm1.java
@@ -0,0 +1,25 @@
+package com.progms;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Progm1 {
+	public static void main(String[] args) {
+
+		// Creating a list of strings
+		List<String> aList = Arrays.asList("Geeks", "for", "GeeksQuiz",
+				"GeeksforGeeks", "GFG");
+
+		Set<String> hSet = new HashSet<String>();
+		for (String x : aList)
+			hSet.add(x);
+
+		System.out.println("Created HashSet is");
+		for (String x : hSet)
+			System.out.println(x);
+
+		// We can created TreeSet same way
+	}
+}
